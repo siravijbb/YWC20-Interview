5e65d3bf48d2a53895b60bc4fc8938bbe6b7c2a5
Siravij Praevisavakij <siravijbb@gmail.com>
2025-05-06 00:12:07 +0700

    fix:footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -80,7 +80,7 @@
 	}
 </script>
 
-<div class="min-w-full min-h-screen bg-black   text-white">
+<div class="min-w-full min-h-[90svh] bg-black   text-white">
 <section  class="bg-black  sticky top-0 ">
 	<nav
 		class=" mx-auto flex max-w-6xl items-center justify-between bg-black px-8 py-4 transition-all"
@@ -162,6 +162,6 @@
 </section>
 
 </div>
-<section class="sm:-mt-40 bottom-1 mx-auto max-w-7xl space-y-4  pt-8 px-8 bg-black text-gray-400  ">
-	<footer id="footer" class="container mx-auto px-8 flex flex-col items-center justify-center  md:grid grid-cols-1 md:grid-cols-7 pb-8"><div class="col-span-1"><a href="https://www.webmaster.or.th/" target="_blank"><img alt="Main logo" loading="lazy" width="63" height="32" decoding="async" data-nimg="1" style="color:transparent" src="https://ywc20.ywc.in.th/twa.svg"></a></div><div class="col-span-1 md:col-span-3 flex gap-8 py-4"><a class="footer-link sm:text-sm" href="https://ywc20.ywc.in.th/privacy-policy">Privacy Policy</a><a class="footer-link sm:text-sm" href="https://ywc20.ywc.in.th/terms">Terms and Conditions</a><a class="footer-link sm:text-sm" href="https://sponsor.ywc.in.th">Sponsorship</a></div><div class="col-span-1 md:col-span-3"><p class="footer-text text-center md:text-right">Copyright 2003 - 2025. Young Webmaster Camp,</p><p class="footer-text text-center md:text-right">in association with Thai Webmaster and Online Media Association, all rights reserved.</p></div></footer>
+<section class="  mx-auto space-y-4  pt-8 px-8 bg-black text-gray-400  ">
+	<footer id="footer" class=" max-w-7xl container mx-auto px-8 flex flex-col items-center justify-center  md:grid grid-cols-1 md:grid-cols-7 pb-8"><div class="col-span-1"><a href="https://www.webmaster.or.th/" target="_blank"><img alt="Main logo" loading="lazy" width="63" height="32" decoding="async" data-nimg="1" style="color:transparent" src="https://ywc20.ywc.in.th/twa.svg"></a></div><div class="col-span-1 md:col-span-3 flex gap-8 py-4"><a class="footer-link sm:text-sm" href="https://ywc20.ywc.in.th/privacy-policy">Privacy Policy</a><a class="footer-link sm:text-sm" href="https://ywc20.ywc.in.th/terms">Terms and Conditions</a><a class="footer-link sm:text-sm" href="https://sponsor.ywc.in.th">Sponsorship</a></div><div class="col-span-1 md:col-span-3"><p class="footer-text text-center md:text-right">Copyright 2003 - 2025. Young Webmaster Camp,</p><p class="footer-text text-center md:text-right">in association with Thai Webmaster and Online Media Association, all rights reserved.</p></div></footer>
 </section>
